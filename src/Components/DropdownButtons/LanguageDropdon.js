46ee42b2e68620bb83d02957c6e073aa7f916616
AliAkbar <masterakbaridev@gmail.com>
2024-07-26 15:40:44 +0330

    eight commit

--- src/Components/DropdownButtons/LanguageDropdon.js
+++ src/Components/DropdownButtons/LanguageDropdon.js
@@ -18,7 +18,7 @@ const LanguageDropdown = ({ languages }) => {
   return (
     <div className='flex justify-center items-center ltr:mr-[29px] rtl:ml-[29px] relative'>
       <button onClick={() => setOpen((prev) => !prev)}><GlobeAmericas className='text-nav' size={25} /></button>
-      <ul className={`shadow-md w-max h-fit absolute border rounded-xl z-10 top-[70px] text-center transition duration-300 bg-white ${isOpen ? "opacity-100 visible" : "opacity-0 invisible"}`}>
+      <ul className={`shadow-md w-max h-fit absolute border rounded-xl z-10 top-[70px] text-center transition duration-300 bg-white overflow-hidden ${isOpen ? "opacity-100 visible" : "opacity-0 invisible"}`}>
         {languages.map((item, index) => {
           return <li key={index} onClick={() => i18n.changeLanguage(item.code)} className={`cursor-pointer w-full py-10px px-4 ${currentLanguage === item.code ? "bg-gray-200" : ""}`}>{item.name}</li>
         })}
