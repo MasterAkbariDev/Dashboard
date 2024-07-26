4a96f70077e93a835295b9f003ce6bb5cb255519
AliAkbar <masterakbaridev@gmail.com>
2024-07-26 05:51:29 +0330

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Sidebar from './Components/Sidebar/Sidebar';
+import Header from './Containers/Header/Header';
+import { SidebarProvider } from './Context/SidebarContext';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import Classic from './Containers/Dashboard/Classic/Classic';
+import Minimal from './Containers/Dashboard/Minimal/Minimal';
+import Login from './Containers/Pages/Login/Login';
+import Register from './Containers/Pages/Register/Register';
+import ForgetPassword from './Containers/Pages/ForgetPassword/ForgetPassword';
+import FAQSection from './Containers/Applications/FAQ/FAQ';
+import Chat from './Containers/Applications/Chat/Chat';
+import MailBox from './Containers/Applications/MailBox/MailBox';
 
 function App() {
+
+  const { i18n } = useTranslation();
+
+  useEffect(() => {
+
+    const updateDirection = () => {
+      if (i18n.language === 'fa') {
+        document.documentElement.dir = 'rtl';
+      } else {
+        document.documentElement.dir = 'ltr';
+      }
+    };
+
+    updateDirection();
+
+    i18n.on('languageChanged', updateDirection);
+
+    return () => {
+      i18n.off('languageChanged', updateDirection);
+    };
+  }, [i18n]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <SidebarProvider>
+          <Sidebar />
+          <main className='lg:ltr:pl-[270px] lg:rtl:pr-[270px] transition-all duration-300'>
+            <Header />
+            <div className='p-30px'>
+              <Routes>
+                <Route path='/' index Component={Classic} />
+                <Route path='/minimal' Component={Minimal} />
+                <Route path='/Login' Component={Login} />
+                <Route path='/Register' Component={Register} />
+                <Route path='/ForgetPassword' Component={ForgetPassword} />
+                <Route path='/Applications/'>
+                  <Route path='MailBox' Component={MailBox} />
+                  <Route path='Chat' Component={Chat} />
+                  <Route path='FAQ' Component={FAQSection} />
+                </Route>
+              </Routes>
+            </div>
+          </main>
+        </SidebarProvider>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
